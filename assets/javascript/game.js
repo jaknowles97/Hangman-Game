dbc25441df4ec60932ca3fd639da9d54828ea68c
Andrew <jaknowles1997@gmail.com>
2018-02-26 19:54:41 -0800

    func-tionality sorta..

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,64 @@
  var wordList = ["a", "bc", "def", "andrew", "knowles", "john", "sergio", "jeff"];
  var graveyard = [];
- 
+ var answerKey = [];
  var currentWord;
  var lettersRemaining;
  var lives = 10;
  var letterSelected;
 
+function startGame() {
+    lives = 10;
+    function getRandWord() {
+        currentWord = wordList[Math.floor(Math.random() * wordList.length)];
 
-currentWord = wordList[Math.floor(Math.random() * wordList.length)];
-
-var answerKey = [];
-for(var i = 0; i < currentWord.length; i++) {
-    answerKey[i] = "_";
+        for(var i = 0; i < currentWord.length; i++) {
+            answerKey[i] = "_";
+        }
+    }
+    getRandWord();
 }
 
+startGame();
+
 lettersRemaining = currentWord.length;
+function updateGame() {
+    if(lettersRemaining <= 0 && lives > 0) {
+        var winHtml =
+        '<h1 class="display-3 text-center"> Woot! You won!!</h1>'+
+        '<h2 class="display-4 text-center"> The word was: '+currentWord+'</h2>'+
+        '<button class="btn btn-dark justify-content-center reset" onclick="startGame()">Play Again</button>';
+        document.querySelector("#game").innerHTML = winHtml;
+        var playAgain = document.getElementsByClassName("reset");
+        playAgain.onclick(startGame());        
+    }else if (lives <= 0) {
+        var winHtml =
+        '<h1 class="display-3 text-center"> Shoot! You lost!!<h1>'+
+        '<h2 class="display-4 text-center"> The word was: '+currentWord+'<h2>'+
+        '<button class="btn btn-dark reset">Play Again<button>';
+        document.querySelector("#game").innerHTML = winHtml;
+    } else {
+        var updateHtml =
+        "<h2> press a key to choose a letter </h2>" +
+        "<h3>word: " + answerKey.join(" ")  + "</p>" +
+        "<h3>letterSelected: " + letterSelected + "</p>" +
+        "<h3>graveyard: " + graveyard + "</p>" +
+        "<h3>lives: " + lives + "</h3>";
+        document.querySelector("#game").innerHTML = updateHtml;
+    }
+   var keyHistory = answerKey;
+}
 
-
+updateGame();
 
 document.onkeyup = function(event){
     letterSelected = event.key;
-    keyHistory = answerKey;
+    var keyHistory;
         //check to see if char is part of current word
     for(var j = 0; j < currentWord.length; j++) {
         if(currentWord[j] === letterSelected) {
             answerKey[j] = letterSelected;
             lettersRemaining--;
+            keyHistory = answerKey;
         }
     }
     if( graveyard.indexOf(letterSelected) < 0)   {
@@ -33,30 +66,8 @@ document.onkeyup = function(event){
     } else {
         letterSelected = "already used letter";
     }
-    if( keyHistory === answerKey) {
+    if( keyHistory != answerKey && graveyard.indexOf(letterSelected) >= 0) {
         lives--;
     }
-    if(lettersRemaining <= 0 && lives > 0) {
-        alert("You won! The coreect word was: " + currentWord); 
-    }else if (lives <= 0) {
-        alert("ya lost. the correct word was: " + currentWord);
-    }
-    
-    if(lives <= 0){
-        
-    }
-
-    var html =
-    "<p>word: " + answerKey + "</p>" +
-    "<p>letterSelected: " + letterSelected + "</p>" +
-    "<p>graveyard: " + graveyard + "</p>";
-
-    document.querySelector("#game").innerHTML = html;
+    updateGame();
 };
-
-
-
-
-
-
-
